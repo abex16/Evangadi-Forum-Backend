dbefd39126ef3cac995614223357b20e55e6f7db
abex16 <abinetkedir@gmail.com>
2024-06-28 15:12:54 -0500

    database changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 //dotenv.config()
 const express = require("express");
 const app = express();
-const port = 5500;
+const port = process.env.PORT;
 
 const cors = require('cors')
 
